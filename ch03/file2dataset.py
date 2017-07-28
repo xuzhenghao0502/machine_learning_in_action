0160c07b773caeb1ed12f5370a244a780f30d4bb
xuzhenghao0502 <xuzhenghao0502@163.com>
2017-07-28 17:12:28 +0800

    give up file2dataset.py -- numpy matrix cannot save string

--- ch03/file2dataset.py
+++ ch03/file2dataset.py
@@ -21,11 +21,55 @@ def file2dataset(filename = ''):
         #read file
         with open(filename) as file_object:
             lines = file_object.readlines()
-            print(lines)
+
+        #delete the space in txt file
+        number_of_datasets = len(lines)
+        lines_no_space = []
+        for line in lines:
+            lines_no_space.append(line.rstrip())
+
+        # split_in_columns = input("By default, the dataset is splitted in rows. If you want to split it in columns, input 'column':")
+        split_in_columns = 'row'
+        # if split_in_columns == "column":
+        #     #split in column
+        #
+        # else:
+            # print("Input which column is the label. Input '-1' to define the last column as the label.")
+            # number_of_label_column = input("Which column is the label: ")
+
+        first_row, number_of_elements = deal_row(lines_no_space[0], ',')
+
+        #create an empty numpy matrix to save the dataset
+        return_mat = np.zeros((number_of_datasets, number_of_elements))
+        mat_row_index = 0
+        for line in lines_no_space:
+            mat_row, number_of_elements = deal_row(lines_no_space[mat_row_index], ',')
+            return_mat[mat_row_index, :] = mat_row[:]
+            mat_row_index += 1
 
         #end process
         input_finish = True
 
+def deal_row(string, delimiter):
+    """return the processed row of data and the number of features of each row"""
+    number_of_element = 0
+    list_of_row = []
+    while len(string):
+        delimiter_position = string.find(delimiter)
+        if delimiter_position != -1:
+            element_to_append = string[0:delimiter_position]
+            if element_to_append != '':
+                list_of_row.append(element_to_append)
+                number_of_element += 1
+                string = string[delimiter_position + 1:]
+        else:
+            if string != '':
+                element_to_append = string
+                list_of_row.append(element_to_append)
+                number_of_element += 1
+                string = []
+
+    return list_of_row,number_of_element
 
 
 file2dataset("tree_dataset.txt")
